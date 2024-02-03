efec474c0e44b7dd6450990dba5ace5112f1fe08
janthony25 <junanthony.business@gmail.com>
2024-02-04 11:32:42 +1300

    Add project files.

---
+++ OOP-ToString2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ToString() override - Practice
+
+            Person person1 = new Person("Jun", "Calupcupan");
+
+            Console.WriteLine(person1);
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        string name;
+        string lastName;
+
+        public Person(string name, string lastName)
+        {
+            this.name = name;
+            this.lastName = lastName;
+        }
+        public override string ToString()
+        {
+            string message = $"hello {name} {lastName}";
+            return message;
+        }
+    }
+}
